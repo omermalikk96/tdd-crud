aca1d4505f3b4d472641a11f8ca40f88e2b6c05f
Omer Nadeem Malik <omer.topdot@gmail.com>
2022-02-22 17:26:42 +0500

    docker files

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,7 +10,7 @@ class BookController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        return view('books.index');
     }
 
     public function create()
